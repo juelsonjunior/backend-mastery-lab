e6abbbc16ab26a39caae3dff378d0cc1450020ce
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-06 07:01:20 +0100

    refactor: Rewrite admin service using object orientation

--- src/service/adminService.js
+++ src/service/adminService.js
@@ -1,35 +1,37 @@
-import userRepository from "../repository/userRepository.js";
-import refreshTokenRepository from "../repository/refreshTokenRepository.js";
 import { blackListToken } from "../utils/tokenBlacklist.js";
+import AppError from "../errors/AppError.js";
 
-const adminService = {
+export default class AdminService {
+  constructor(userRepository, refreshTokenRepository) {
+    this.userRepository = userRepository;
+    this.refreshTokenRepository = refreshTokenRepository;
+  }
+  
   async disabledUser(userId, { active }, reqUserId) {
-    if (!userId) throw new Error("Precisa informar um id válido");
-
-    if (!reqUserId) throw new Error("ID do usuário autenticado não fornecido");
+    if (!userId) throw new AppError("Precisa informar um id válido");
+    if (!reqUserId)
+      throw new AppError("ID do usuário autenticado não fornecido");
 
     if (active !== true && active !== false)
-      throw new Error("O campo active é obrigatório e deve ser true ou false");
+      throw new AppError(
+        "O campo active é obrigatório e deve ser true ou false"
+      );
 
-    const user = await userRepository.findById(userId);
-
-    if (!user) {
-      throw new Error("Nenhum usuário foi encontrado");
-    }
+    const user = await this.userRepository.findById(userId);
+    if (!user) throw new AppError("Nenhum usuário foi encontrado");
 
     if (user._id.toString() == reqUserId.toString())
-      throw new Error("Você não pode desativar tua propria conta");
+      throw new AppError("Você não pode desativar tua propria conta");
 
     if (user.role == "admin" || user.role == "mod")
-      throw new Error(
+      throw new AppError(
         "Não é permitido desativar usuários com privilégios elevados"
       );
 
-    const disabledUser = await userRepository.update(user._id, { active });
+    const disabledUser = await this.userRepository.update(user._id, { active });
 
-    const tokenUserDisabled = await refreshTokenRepository.findTokenByIdUser(
-      disabledUser._id
-    );
+    const tokenUserDisabled =
+      await this.refreshTokenRepository.findTokenByIdUser(disabledUser._id);
 
     if (tokenUserDisabled && tokenUserDisabled.length > 0) {
       tokenUserDisabled.forEach(({ token }) => blackListToken(token));
@@ -38,7 +40,5 @@ const adminService = {
     return {
       message: `Usuario ${active ? "Activado" : "Desativado"} com sucesso`,
     };
-  },
-};
-
-export default adminService;
+  }
+}
