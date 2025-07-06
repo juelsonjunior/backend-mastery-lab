8324bfe3e1b2fd91c1cbf26ffdc6f5a12188cfa1
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-06 07:01:41 +0100

    refactor: Rewrite user service using object orientation

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,27 +1,32 @@
-import userRepository from "../repository/userRepository.js";
 import bcrypt from "bcrypt";
-import refreshTokenRepository from "../repository/refreshTokenRepository.js";
 import { blackListToken } from "../utils/tokenBlacklist.js";
+import AppError from "../errors/AppError.js";
 
-const userService = {
+export default class UserService {
+  constructor(userRepository, refreshTokenRepository) {
+    this.userRepository = userRepository;
+    this.refreshTokenRepository = refreshTokenRepository
+  }
+
   async profile(userId, { name, email }) {
-    if (!userId) throw new Error("ID do usuário autenticado não fornecido");
+    if (!userId) throw new AppError("ID do usuário autenticado não fornecido");
+    if (!name) throw new AppError("Precisa preencher o campo nome");
+    if (!email) throw new AppError("Precisa preencher o campo email");
 
-    if (!name) throw new Error("Precisa preencher o campo nome");
+    const user = await this.userRepository.findById(userId);
+    if (!user) throw new AppError("usuáro não encontrado");
 
-    if (!email) throw new Error("Precisa preencher o campo email");
-
-    const user = await userRepository.findById(userId);
-    if (!user) throw new Error("usuáro não encontrado");
-
-    const userEmailExists = await userRepository.findByEmail(email);
+    const userEmailExists = await this.userRepository.findByEmail(email);
     if (userEmailExists && userEmailExists._id.toString() !== userId.toString())
-      throw new Error("Este email já está em uso por outro usuário");
+      throw new AppError("Este email já está em uso por outro usuário");
 
-    const updateduser = await userRepository.update(userId, { name, email });
+    const updateduser = await this.userRepository.update(userId, {
+      name,
+      email,
+    });
 
     return { message: "Usuario atualizado", data: updateduser };
-  },
+  }
 
   async updatedPassword(token, { userId, oldPassword, newPassword }) {
     if (token) {
@@ -29,58 +34,54 @@ const userService = {
     }
 
     if (!oldPassword || !newPassword)
-      throw new Error("Precisa preencher todos os campos");
-
+      throw new AppError("Precisa preencher todos os campos");
     if (newPassword.length < 5)
-      throw new Error("Senha precisa ter no minimo 5 caracteres");
+      throw new AppError("Senha precisa ter no minimo 5 caracteres");
 
-    const user = await userRepository.findById(userId);
-    if (!user) throw new Error("usuáro não encontrado");
+    const user = await this.userRepository.findById(userId);
+    if (!user) throw new AppError("usuáro não encontrado");
 
     const isPasswordCorrect = await bcrypt.compare(oldPassword, user.password);
     if (!isPasswordCorrect)
-      throw new Error("A senha atual esta errada. Tente novamente!");
-
+      throw new AppError("A senha atual esta errada. Tente novamente!");
     if (newPassword == oldPassword)
-      throw new Error("A nova senha não pode ser igual a antiga");
+      throw new AppError("A nova senha não pode ser igual a antiga");
 
     const hashPassword = await bcrypt.hash(newPassword, 10);
-    await userRepository.update(user._id, { password: hashPassword });
+    await this.userRepository.update(user._id, { password: hashPassword });
 
-    return { message: "Senha atualizada com sucesso" };
-  },
+    return { message: "Senha atualizada com sucesso. Faça login novamente" };
+  }
 
   async deleteAccount({ id, password }) {
-    if (!password) throw new Error("Precisa preencher o campo senha");
+    if (!password) throw new AppError("Precisa preencher o campo senha");
 
-    const user = await userRepository.findById(id);
-    if (!user) throw new Error("Nenhum usuario encontrado");
+    const user = await this.userRepository.findById(id);
+    if (!user) throw new AppError("Nenhum usuario encontrado");
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect)
-      throw new Error("Senha errada. Digite a senha correta");
+      throw new AppError("Senha errada. Digite a senha correta");
 
-    const deleted = await userRepository.delete(id);
-    if (!deleted) throw new Error("Falha ao eliminar a conta");
+    const deleted = await this.userRepository.delete(id);
+    if (!deleted) throw new AppError("Falha ao eliminar a conta");
 
-    await refreshTokenRepository.deleteByUserId(user._id);
+    await this.refreshTokenRepository.deleteByUserId(user._id);
 
     return { message: "Conta eliminada com sucesso" };
-  },
+  }
 
   async listRefreshTokens(userId) {
-    if (!userId) throw new Error("ID do usuário autenticado não fornecido");
+    if (!userId) throw new AppError("ID do usuário autenticado não fornecido");
 
-    const user = await userRepository.findById(userId);
-    if (!user) throw new Error("usuáro não encontrado");
+    const user = await this.userRepository.findById(userId);
+    if (!user) throw new AppError("usuáro não encontrado");
 
-    const listTokens = await refreshTokenRepository.findTokenByIdUser(userId);
+    const listTokens = await this.refreshTokenRepository.findTokenByIdUser(userId);
 
     if (!listTokens || listTokens.length == 0)
-      throw new Error("Nenhum token encontrado");
+      throw new AppError("Nenhum token encontrado");
 
     return { message: "Seus tokens de refresh", data: listTokens };
-  },
-};
-
-export default userService;
+  }
+}
