13820fb3deab95a6d5f3001bb6e116cd34e65552
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-06 06:52:55 +0100

    refactor: Rewrite admin controller using object orientation

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,21 +1,29 @@
-import adminService from "../service/adminService.js";
+import BaseController from "./baseController.js";
 
-const adminController = {
-  async disabledUser(req, res) {
-    try {
-      const result = await adminService.disabledUser(req.params.id, req.body, req.user.userId);
-      res.status(200).json(result);
-    } catch (err) {
-      res.status(500).json({ message: "Erro interno", error: err.message });
-    }
-  },
+export default class AdminController extends BaseController {
+  constructor(adminService) {
+    super()
+    this.adminService = adminService;
 
-  async dashBoardAdmin(req, res) {
+    this.disabledUser = this.handlerRequest(
+      this.disabledUserHanlder.bind(this)
+    );
+    this.homeAdmin = this.homeAdmin.bind(this);
+  }
+  
+  async homeAdmin(req, res) {
     res.status(200).json({
       message: `Acesso autorizado. Bem-vindo usuário admin`,
       data: req.user,
     });
-  },
-};
+  }
 
-export default adminController;
+  async disabledUserHanlder(req) {
+    return await this.adminService.disabledUser(
+      req.params.id,
+      req.body,
+      req.user.userId
+    );
+  }
+
+}
