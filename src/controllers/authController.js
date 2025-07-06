09ef3c8dd930e0698fa6dd104d26f0005bc311e4
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-06 06:53:06 +0100

    refactor: Rewrite auth controller using object orientation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,59 +1,52 @@
-import authService from "../service/authService.js";
+import BaseController from "./baseController.js";
 
-const authController = {
-  async login(req, res) {
-    try {
-      const { refreshToken, accessToken } = await authService.login({
-        email: req.body.email,
-        password: req.body.password,
-        userAgent: req.headers["user-agent"],
-        ip: req.ip,
-      });
-      res
-        .cookie("refreshToken", refreshToken, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "Strict",
-          maxAge: 7 * 24 * 60 * 60 * 1000,
-        })
-        .json({ accessToken });
-    } catch (err) {
-      res.status(500).json({ message: "Erro interno", error: err.message });
-    }
-  },
+export default class AuthController extends BaseController {
+  constructor(authService) {
+    super();
+    this.authService = authService;
+    this.login = this.handlerRequest(this.loginHandler.bind(this));
+    this.register = this.handlerRequest(this.registerHandler.bind(this));
+    this.refresh = this.handlerRequest(this.refreshHandler.bind(this));
+    this.logout = this.handlerRequest(this.logoutHandler.bind(this));
+  }
 
-  async register(req, res) {
-    try {
-      const result = await authService.register(req.body);
-      res.status(201).json(result);
-    } catch (err) {
-      res.status(500).json({ message: "Erro interno", error: err.message });
-    }
-  },
+  async loginHandler(req, res) {
+    const { refreshToken, accessToken } = await this.authService.login({
+      email: req.body.email,
+      password: req.body.password,
+      userAgent: req.headers["user-agent"],
+      ip: req.ip,
+    });
 
-  async refresh(req, res) {
-    try {
-      const result = await authService.refresh(req.cookies.refreshToken);
-      res.status(200).json(result);
-    } catch (err) {
-      res.status(500).json({ message: "Erro interno", error: err.message });
-    }
-  },
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return accessToken;
+  }
 
-  async logout(req, res) {
+  async registerHandler(req, res) {
+    return await this.authService.register(req.body);
+  }
+
+  async refreshHandler(req, res) {
+    return await this.authService.refresh(req.cookies.refreshToken);
+  }
+
+  async logoutHandler(req, res) {
     const token = req.headers.authorization.split(" ")[1];
-    try {
-      const result = await authService.logout(token, req.cookies.refreshToken);
-      res.clearCookie("refreshToken", {
-        httpOnly: true,
-        secure: true,
-        sameSite: "Strict",
-      });
-      res.status(200).json(result);
-    } catch (err) {
-      res.status(500).json({ message: "Erro interno", error: err.message });
-    }
-  },
-};
 
-export default authController;
+    const result = await this.authService.logout(
+      token,
+      req.cookies.refreshToken
+    );
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+    });
+    return result;
+  }
+}
