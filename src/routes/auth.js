11d6752c7f093fa2d43b4357cb2e158479f8f3cc
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-06 06:59:45 +0100

    refactor: use AuthController instance via container

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import authController from "../controllers/authController.js";
+import { authController } from "../container.js";
 
 const authRouter = Router();
 
