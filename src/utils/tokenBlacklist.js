ef9e4224cf13b30f15334d16546a2775b7684e54
juelsonjunior <juelsonjunior27@gmail.com>
2025-07-06 07:04:01 +0100

    refactor: ajust time of blackList the token revoked

--- src/utils/tokenBlacklist.js
+++ src/utils/tokenBlacklist.js
@@ -1,6 +1,6 @@
 import redis from "../database/redis.js";
 
-export const blackListToken = async (token, ttl = 15 * 60) => {
+export const blackListToken = async (token, ttl = 15 * 60 * 1000) => {
   await redis.set(`bl:${token}`, "revoked", "PX", ttl);
   console.log(`Token adicionado a blackList com ttl de ${ttl}ms`);
 };
